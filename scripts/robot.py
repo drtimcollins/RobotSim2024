90fe03691c758acb1e1eaa1b1e866d0258b081b3
Tim Collins <47898052+drtimcollins@users.noreply.github.com>
2023-10-20 11:57:42 +0100

    Updates to robot module

--- scripts/robot.py
+++ scripts/robot.py
@@ -3,17 +3,32 @@ import browser
 #speed = [0, 0]
 an = [0]*10
 
-def motor(i, x):
-	sp = float(int(x) & 0xFFFF)
-	browser.window.speed[i] = sp / 128000.0
+class _MotorOutput:
+	def __init__(self, index):
+		self.i = index
+	def speed(self, x):
+		sp = float(int(x) & 0xFFFF)
+		browser.window.speed[self.i] = sp / 128000.0	
+motor = (_MotorOutput(0), _MotorOutput(1))
 def motors(x, y):
-	motor(0, x)
-	motor(1, y)
+    motor[0].speed(x)
+    motor[1].speed(y)
 
-def readsensor(i):
-	return an[i]
+class _SensorInput:
+	def __init__(self, index):
+		self.i = index
+	def read(self):
+		return an[self.i]
 def readsensors(i):
 	return an[0:i]
+sensor = ()
+
+def _initialise():
+    global sensor
+    for n in range(6):
+        sensor = sensor + (_SensorInput(n),)
+
+_initialise()
 
 browser.window.timercallback = None
 browser.window.timerfreq = None
